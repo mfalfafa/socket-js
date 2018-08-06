9af601246bb079a1cba7fa38a0ba0b282f4e893d
FF077 <miftahf77@gmail.com>
2018-08-06 13:16:56 +0700

    TCP Client in NodeJS

--- node-client.js
+++ node-client.js
@@ -1,11 +1,19 @@
 var net = require('net');
 
 var client = new net.Socket();
+var i=0
 
 function connect_(){
 	client.connect(5000, '192.168.10.250', function() {
 		console.log('Connected');
-		client.write('Hello, server! Love, Client.');
+	});
+}
+
+function ready_(){
+	client.on('ready'm function(){
+		i++;
+		client.write(i.toString());
+		setInterval(ready_, 1*1000);
 	});
 }
 
